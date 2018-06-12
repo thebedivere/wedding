9d505cfb0883aff840c7531ab441b376e2c41e13
Josh Derocher <joshua.aarond@gmail.com>
2018-06-12 15:53:41 -0400

    Added totals

--- components/Guests.js
+++ components/Guests.js
@@ -1,14 +1,31 @@
 import { compose, lifecycle, withState, withHandlers } from 'recompose'
 import Guest from './Guest'
+import * as R from 'ramda'
 import '../style/main.css'
+
+const countTotals = guests => {
+  const no = [].concat(...guests.map(g =>
+    g.members.map(m => {
+      if (m.attending === 'No') return m.name
+    }).filter(i => i)
+  )).length
+  const yes = [].concat(...guests.map(g =>
+    g.members.map(m => {
+      if (m.attending === 'Yes') return m.name
+    }).filter(i => i)
+  )).length
+  return { yes, no, total: yes + no }
+}
 
 const withData = lifecycle({
   componentDidMount () {
     this.props.db.collection('guests').onSnapshot(
       querySnapshot => {
         const guests = querySnapshot.docs.map(doc => doc.data())
+
         this.setState({
-          guests
+          guests,
+          total: countTotals(guests)
         })
       }
     )
@@ -17,6 +34,7 @@ const withData = lifecycle({
 
 const enhance = compose(
   withState('guests', 'updateGuests', []),
+  withState('total', 'updateTotal', {yes: 0, no: 0, total: 0}),
   withState('lastName', 'updateLastName', ''),
   withHandlers({
     handleLastName: props => event => {
@@ -31,7 +49,7 @@ const enhance = compose(
   withData
 )
 
-const Guests = enhance(({guests, lastName, handleLastName, handleCreateGuest, db}) => (
+const Guests = enhance(({guests, lastName, handleLastName, handleCreateGuest, db, total}) => (
   <div style={{ paddingTop: '50px' }}>
     <form onSubmit={handleCreateGuest} style={{ position: 'fixed', width: '100%', top: 0, background: 'white', padding: '5px', zIndex: 100 }}>
       <label>Last Name</label>
@@ -40,6 +58,11 @@ const Guests = enhance(({guests, lastName, handleLastName, handleCreateGuest, db
       <button type='submit'>Add new family</button>
     </form>
     {guests.map(g => <Guest guest={g} key={g.lastname + Math.random().toString()} db={db} />)}
+    <p>
+    Total {total.total}<br />
+    Yes {total.yes}<br />
+    No {total.no}
+    </p>
   </div>
 ))
 
