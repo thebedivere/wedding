613dd98f6a778952fe1ee6cf631876cc00dc37a3
Josh Derocher <joshua.aarond@gmail.com>
2018-06-14 18:36:08 -0400

    fixed font console error

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
           <meta name='description' content='joshua and roxane wedding website' />
           <meta name='viewport' content='width=device-width, initial-scale=1, minimal-ui' />
           <link rel='stylesheet' href='/_next/static/style.css' />
-          <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,700|Niconne|Heebo|Montserrat|Arimo|Pacifico|Roboto|Bree+serif|Questrial|Catamaran' rel='stylesheet' type='text/css' />
+          <link href='//fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,700|Niconne|Heebo|Montserrat|Arimo|Pacifico|Roboto|Bree+serif|Questrial|Catamaran' rel='stylesheet' type='text/css' />
           <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossorigin='anonymous' />
           <link rel='stylesheet' href='/_next/static/style.css' />
         </Head>
